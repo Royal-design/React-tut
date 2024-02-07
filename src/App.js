231b5dac07006f4c8546980cb10b7b9123eff999
Royal-design <royaldesign1999@gmail.com>
2024-02-07 08:31:20 +0100

    updated the files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+import { Home } from "./Home";
+import useAxiosFetcher from "./hooks/useAxiosFetcher";
 
 function App() {
+  const [ingredients, setIngredients] = useState([]);
+  const { data, isLoading, fetchError } = useAxiosFetcher(
+    "https://www.themealdb.com/api/json/v1/1/search.php?s=vegetable"
+  );
+  useEffect(() => {
+    setIngredients(data);
+  }, [data]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      </div>
+      <Home
+        ingredients={ingredients}
+        isLoading={isLoading}
+        fetchError={fetchError}
+      />
+    </div>
+  );
+}
+
+export default App;
